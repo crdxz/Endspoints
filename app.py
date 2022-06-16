f88ad3675221b8d570ff3d8f831f275f35775c57
Yeisson Cardozo <61598547+crdxz@users.noreply.github.com>
2022-06-16 17:07:27 -0500

    Nuevo endpoint

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_word():
     return "<p>Hello, World!</p>" 
+@app.route("/goodbye")
+def bye_world():
+    return "<p>Good bye, World!</p>" 
 
-def kekw():
-    return none 
+
